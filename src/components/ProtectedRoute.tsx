f56a314d77d3567ddc928c5221e1ac21fdad3542
ian107x <imjrowland@gmail.com>
2024-05-21 16:49:27 +0100

    first commits

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react"
+import { useDispatch } from "react-redux"
+
+type props = {
+    children: ReactNode
+}
+
+export default function ProtectedRoute({children}: props){
+    const dispatch = useDispatch()
+}
